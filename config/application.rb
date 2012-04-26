09f69c867ec4c7a9e35bada3b05eff24aed3b670
Wasim Akram <mailwasim@gmail.com>
2012-04-26 07:51:19 +0500

    asset piple isse

--- config/application.rb
+++ config/application.rb
@@ -69,7 +69,7 @@ module Vote
     # Enable the asset pipeline
     config.assets.enabled = true
 
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
