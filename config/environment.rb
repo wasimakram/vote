eed9d4839eb4ec49888817b71c3ffaad2895e124
Wasim Akram <mailwasim@gmail.com>
2012-04-26 13:28:48 +0500

    testing a different way of loading configerations

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-if Rails.env == 'production'
-  APP_CONFIG = YAML.load_file(Rails.root.join('config', 'config.yml'))['production']
-end
-
-
 # Initialize the rails application
 Vote::Application.initialize!
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,7 @@
 Vote::Application.configure do
 
+  APP_CONFIG = YAML.load_file(Rails.root.join('config', 'config.yml'))[Rails.env]
+
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
